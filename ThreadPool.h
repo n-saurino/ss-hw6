e10c28469fea6b09c0101041d6600e0fad194257
Nigel Saurino <nigelsaurino@gmail.com>
2024-08-05 15:24:12 -0500

    Finished first draft of ThreadPool. Need to test it.

--- ThreadPool.h
+++ ThreadPool.h
@@ -22,37 +22,93 @@ class ThreadPool
 {
 private:
     /* data */
-    std::atomic_bool is_active = false;
-    std::vector<std::thread> threads;
-    std::deque<std::function<void()>> pending_jobs;
-    std::mutex mtx;
-    std::condition_variable cv;
-
-public:
-    ThreadPool();
-    ~ThreadPool();
-    
-    bool Busy(){
-
-    }
+    std::atomic_bool is_active_{true};
+    std::vector<std::thread> threads_;
+    std::deque<std::function<void()>> pending_jobs_;
+    std::mutex mtx_;
+    std::condition_variable cv_;
 
     void Run(){
-        
+        // perpetual while loop
+        while(true){
+
+            // declare a job
+            std::function<void()> job;
+
+            // create a new scope with {}
+            {
+                // grab the lock
+                std::unique_lock<std::mutex> uniq_lock(mtx_);
+                
+                // set this thread to wait on the lock and make sure that the q is not empty 
+                // and that the threadpool is still active
+                cv_.wait(uniq_lock, [this]{return !pending_jobs_.empty() || !is_active_;});
+
+                // if !is_active_ -> null
+                if(!is_active_){
+                    return;
+                }
+
+                // set job = front of pending_jobs
+                job = pending_jobs_.front();
+
+                // pop pending_jobs
+                pending_jobs_.pop_front();
+            }
+            job();
+        }
+    }
+
+public:
+    ThreadPool(){
+        Start();
+    }
+
+    ~ThreadPool(){
+        Stop();
+    }
+    
+    bool Busy(){
+        bool pool_busy;
+        {
+            std::unique_lock<std::mutex> uniq_lock(mtx_);
+            pool_busy = !pending_jobs_.empty();
+        }
+        return pool_busy;
     }
 
     void Start(){
         const uint32_t num_threads = std::thread::hardware_concurrency(); // gets the number of threads supported by system
         for(uint32_t i = 0; i < num_threads; ++i){
-            threads.emplace_back(std::thread(&ThreadPool::Run, this));
+            threads_.emplace_back(std::thread(&ThreadPool::Run, this));
         }
     }
 
     void Stop(){
+        {
+            std::unique_lock<std::mutex> uniq_lock(mtx_);
+            is_active_ = false;
+        }
+        // need to notify all threads that are waiting
+        cv_.notify_all();
 
+        // need to set each thread in thread pool to join
+        for(std::thread& t : threads_){
+            t.join();
+        }
+
+        threads_.clear();
     }
 
+    // add a new job to the queue
+    // use a lock to make sure there isn't a data race
+    // then notify a waiting thread with the conditional variable
     void QueueJob(const std::function<void()>& job){
-
+        {
+            std::unique_lock<std::mutex> uniq_lock(mtx_);
+            pending_jobs_.push_back(job);
+        }
+        cv_.notify_one();
     }
 
 };
