d45d4277d37e6979fef205049614b2f6cfb7c551
Nigel Saurino <nigelsaurino@gmail.com>
2024-08-05 15:47:39 -0500

    Finished initial version of ThreadPool. Need to look into a version that uses futures and promises

--- ex1.cpp
+++ ex1.cpp
@@ -1,6 +1,26 @@
 #include "ThreadPool.h"
+#include <iostream>
 
-int ex1(){
+int Ex1(){
+    int sum = 0;
+    ThreadPool* thread_pool = new ThreadPool;
 
+    // Queue 10 jobs that will increment the sum variable
+    // sum should equal 10 at the end of the task queue
+    for(int i = 0; i < 10; ++i){
+        thread_pool->QueueJob([&sum] {sum++;});
+    }
+
+    // to make sure that the ThreadPool has completed the entire job queue
+    // before we call destructor
+    while(thread_pool->Busy()){
+        continue;
+    }
+
+    // call ThreadPool destructor
+    delete thread_pool;
+    
+    // print to stdout
+    std::cout << sum << std::endl;
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
+#include "ex1.cpp"
 int main(int, char**){
-    int x = 42;
-    printf("Hello, from ss-hw6! Variable x = %d\n", x);
+    Ex1();
 }
