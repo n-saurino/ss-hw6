fe3b226bd09da0f94b60a080a7e1921dc8ad6e4c
Nigel Saurino <nigelsaurino@gmail.com>
2024-08-06 12:50:23 -0500

    Built matrix multiplier. Could optimize for performance in the methodology chosen. First going to see how use of threadpool impacts performance.

--- ex2.cpp
+++ ex2.cpp
@@ -1,5 +1,7 @@
 #include "ThreadPool.h"
 #include <iostream>
+#include <random>
+
 /*
 Part 2: Parallelizing a Computational Task
 
@@ -19,7 +21,7 @@ PMatrix BuildMatrix(int n){
 
     for(int i = 0; i < n; ++i){
         for(int j = 0; j < n; ++j){
-            (*new_matrix)[i][j] = i + j;
+            (*new_matrix)[i][j] = rand() % 10;
         }
     }
 
@@ -36,11 +38,7 @@ void PrintMatrix(PMatrix matrix){
     }
 }
 
-int MatrixMultiply(PMatrix pmatrix1, PMatrix pmatrix2){
-    return 0;
-}
-
-PMatrix TransposeMatrix(PMatrix matrix){
+void TransposeMatrix(PMatrix matrix){
     int n = (*matrix).size();
     for(int i = 0; i < n; ++i){
         for(int j = i; j < n; ++j){
@@ -53,27 +51,51 @@ PMatrix TransposeMatrix(PMatrix matrix){
             (*matrix)[j][i] = temp;
         }
     }
-
-    return matrix;
 }
 
-int VectorMultiply(){
-    return 0;
+int VectorMultiply(const std::vector<int>& vec1, const std::vector<int>& vec2){
+    int n = vec1.size();
+    int result = 0;
+    for(int i = 0; i < n; ++i){
+        result += vec1[i] * vec2[i];
+    }
+
+    return result;
 }
 
+PMatrix MatrixMultiply(PMatrix pmatrix1, PMatrix pmatrix2){
+    // start threads to handle vector multiplication and then combine into a final matrix
+    // probably need a mutex to lock the final matrix and make adjustments to it? Although
+    // the indices that are changed will be different for every thread
+    int n = (*pmatrix1).size();
+    PMatrix new_matrix = new std::vector<std::vector<int>>;
+    new_matrix->resize(n,std::vector<int>(n,0));
+
+    TransposeMatrix(pmatrix2);
+
+    for(int i = 0; i < n; ++i){
+        for(int j = 0; j < n; ++j){
+            (*new_matrix)[i][j] = VectorMultiply((*pmatrix1)[i], (*pmatrix2)[j]);
+        }
+    }
+
+    return new_matrix;
+}
 
 int Ex2(){
 
-    PMatrix pmatrix1 = BuildMatrix(2);
-    PMatrix pmatrix2 = BuildMatrix(2);
+    PMatrix pmatrix1 = BuildMatrix(3);
+    PMatrix pmatrix2 = BuildMatrix(3);
+
+    // print original matrices
+    PrintMatrix(pmatrix1);
+    std::cout << std::endl;
+    PrintMatrix(pmatrix2);
+    std::cout << std::endl;
     
-    (*pmatrix1)[0][1] = 3;
-
-    PrintMatrix(pmatrix1);
-
-    TransposeMatrix(pmatrix1);
-
-    PrintMatrix(pmatrix1);
+    PMatrix pfinal_matrix = MatrixMultiply(pmatrix1, pmatrix2);
+    // print final matrix
+    PrintMatrix(pfinal_matrix);
 
     return 0;
 }
