21b066d21d8e42bab5675326dc40963071607385
Nigel Saurino <nigelsaurino@gmail.com>
2024-08-06 13:58:53 -0500

    Finished week 6 ex2.

--- ex2.cpp
+++ ex2.cpp
@@ -14,6 +14,7 @@ Collect and combine the results of the sub-tasks to obtain the final result.
 */
 
 typedef std::vector<std::vector<int>>* PMatrix;
+typedef std::vector<std::vector<std::future<int>>> FutureMatrix;
 
 PMatrix BuildMatrix(int n){
     PMatrix new_matrix = new std::vector<std::vector<int>>;
@@ -63,15 +64,18 @@ int VectorMultiply(const std::vector<int>& vec1, const std::vector<int>& vec2){
     return result;
 }
 
-PMatrix MatrixMultiply(PMatrix pmatrix1, PMatrix pmatrix2){
+PMatrix MatrixMultiply(const PMatrix pmatrix1, const PMatrix pmatrix2){
     // start threads to handle vector multiplication and then combine into a final matrix
     // probably need a mutex to lock the final matrix and make adjustments to it? Although
     // the indices that are changed will be different for every thread
+
     int n = (*pmatrix1).size();
     PMatrix new_matrix = new std::vector<std::vector<int>>;
     new_matrix->resize(n,std::vector<int>(n,0));
 
     TransposeMatrix(pmatrix2);
+
+     auto start = std::chrono::system_clock::now();
 
     for(int i = 0; i < n; ++i){
         for(int j = 0; j < n; ++j){
@@ -79,23 +83,83 @@ PMatrix MatrixMultiply(PMatrix pmatrix1, PMatrix pmatrix2){
         }
     }
 
+    // std::this_thread::sleep_for(std::chrono::nanoseconds(10));
+
+    auto end = std::chrono::system_clock::now();
+    // this constructs a duration object using milliseconds
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Single thread solution: " << elapsed.count() << " milliseconds\n\n";
+
+    // retransposing matrix to return it to it's original state
+    TransposeMatrix(pmatrix2);
+
+    return new_matrix;
+}
+
+PMatrix ThreadedMatrixMultiply(const PMatrix pmatrix1, const PMatrix pmatrix2){
+    // start threads to handle vector multiplication and then combine into a final matrix
+    // probably need a mutex to lock the final matrix and make adjustments to it? Although
+    // the indices that are changed will be different for every thread
+
+    int n = (*pmatrix1).size();
+    PMatrix new_matrix = new std::vector<std::vector<int>>;
+    new_matrix->resize(n,std::vector<int>(n,0));
+
+    TransposeMatrix(pmatrix2);
+    ThreadPool* thread_pool = new ThreadPool;
+    std::vector<std::future<void>> futures;
+
+     auto start = std::chrono::system_clock::now();
+
+    for (int i = 0; i < n; ++i) {
+        futures.push_back(thread_pool->QueueJob([i, pmatrix1, pmatrix2, new_matrix] {
+            for (int j = 0; j < new_matrix->size(); ++j) {
+                (*new_matrix)[i][j] = VectorMultiply((*pmatrix1)[i], (*pmatrix2)[j]);
+            }
+        }));
+    }
+    
+    for(auto& future : futures){
+        future.get();
+    }
+
+    auto end = std::chrono::system_clock::now();
+    
+    // this constructs a duration object using milliseconds
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "ThreadPool solution: " << elapsed.count() << " milliseconds\n\n";
+
+    // retransposing matrix to return it to its original state
+    TransposeMatrix(pmatrix2);
+    delete thread_pool;
     return new_matrix;
 }
 
 int Ex2(){
 
-    PMatrix pmatrix1 = BuildMatrix(3);
-    PMatrix pmatrix2 = BuildMatrix(3);
+    PMatrix pmatrix1 = BuildMatrix(1000);
+    PMatrix pmatrix2 = BuildMatrix(1000);
 
     // print original matrices
+    /*
     PrintMatrix(pmatrix1);
     std::cout << std::endl;
     PrintMatrix(pmatrix2);
     std::cout << std::endl;
-    
-    PMatrix pfinal_matrix = MatrixMultiply(pmatrix1, pmatrix2);
-    // print final matrix
-    PrintMatrix(pfinal_matrix);
+    */
 
+    PMatrix psingle_matrix = MatrixMultiply(pmatrix1, pmatrix2);
+    // print final matrix
+    // PrintMatrix(psingle_matrix);
+
+    PMatrix pthreaded_matrix = ThreadedMatrixMultiply(pmatrix1, pmatrix2);
+    // print final matrix
+    // PrintMatrix(pthreaded_matrix);
+
+    delete pmatrix1;
+    delete pmatrix2;
+    delete psingle_matrix;
+    delete pthreaded_matrix;
+    
     return 0;
 }
